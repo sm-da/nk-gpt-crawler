41975c2e0f57ac2ee4d7fc6a5539e9bc73bf85e6
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-12 23:39:13 +0000

    Update file

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "http:/",
-  match: "http://**",
+  url: "http://www.airkoryo.com.kp/",
+  match: "http://www.airkoryo.com.kp/**",
   maxPagesToCrawl: 99999999999,
   outputFileName: "air-koryo.json",
   maxTokens: 2000000,
