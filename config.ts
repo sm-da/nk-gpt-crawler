a0686b446dcec18a90be0a13fe4a8c7b538c7a1c
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-12 23:31:27 +0000

    sm FAILURE

--- config.ts
+++ config.ts
@@ -2,9 +2,9 @@ import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
   url: "http:/",
-  match: "https://**",
+  match: "http://**",
   maxPagesToCrawl: 99999999999,
-  outputFileName: "air-korea.json",
+  outputFileName: ".json",
   maxTokens: 2000000,
   selector: "",
 };
