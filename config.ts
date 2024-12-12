e93e6d2f150b025df8fe5dff1c3767c668810311
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-12 23:30:50 +0000

    Update file

--- config.ts
+++ config.ts
@@ -1,9 +1,10 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
-  maxPagesToCrawl: 50,
-  outputFileName: "output.json",
+  url: "http:/",
+  match: "https://**",
+  maxPagesToCrawl: 99999999999,
+  outputFileName: "air-korea.json",
   maxTokens: 2000000,
+  selector: ""
 };
