b9b4aef9c920d46395405e2e001092e14492707b
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-12 23:57:59 +0000

    Update file

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "http://www.chochong.net/*",
+  url: "http://www.chochong.net/",
   match: "http://www.chochong.net/**",
-  maxPagesToCrawl: 99999999999,
+  maxPagesToCrawl: 9999999999,
   outputFileName: "chochong.json",
   maxTokens: 2000000,
   selector: "body",
