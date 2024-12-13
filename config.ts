e5ef04f8bc53edc14e2ce39aa3622b0af741a43b
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 08:16:14 +0000

    uf

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "https://flyinghorsedelta.com/",
   match: "https://flyinghorsedelta.com/**",
-  maxPagesToCrawl: 99999999999,
+  maxPagesToCrawl: 9999999999,
   outputFileName: "flying-horse.json",
   maxTokens: 2000000,
   selector: "body",
