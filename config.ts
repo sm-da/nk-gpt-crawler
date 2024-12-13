0b8f2fba28b9e0274e38b9a68a5ed738311a0764
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 00:48:24 +0000

    uf

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "http://www.korelcfund.org.kp/	",
   match: "http://www.korelcfund.org.kp/**",
-  maxPagesToCrawl: 99999999999,
+  maxPagesToCrawl: 9999999999,
   outputFileName: "korelcfund.json",
   maxTokens: 2000000,
   selector: "body",
