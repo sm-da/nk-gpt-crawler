69827c3619d86f0d43fe6781d024256754db2fa3
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 01:18:50 +0000

    uf

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "http://www.dprk-book.com/",
   match: "http://www.dprk-book.com/**",
-  maxPagesToCrawl: 99999999999,
+  maxPagesToCrawl: 999999999,
   outputFileName: "dprk.json",
   maxTokens: 2000000,
   selector: "body",
