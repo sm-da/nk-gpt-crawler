c8fdf28a8c6af775c51929dd57e6ef3a0d85c8fd
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 01:40:44 +0000

    uf

--- config.ts
+++ config.ts
@@ -1,10 +1,10 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "http:/",
-  match: "http://**",
+  url: "http://naenara.com.kp/",
+  match: "http://naenara.com.kp/**",
   maxPagesToCrawl: 99999999999,
-  outputFileName: ".json",
+  outputFileName: "naenara.json",
   maxTokens: 2000000,
   selector: "body",
 };
