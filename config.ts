4d249a895950b3bddf7fdc67c9636b339b8709cc
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 01:58:03 +0000

    uf

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "http://www.north-korea-books.com/",
   match: "http://www.north-korea-books.com/**",
-  maxPagesToCrawl: 99999999999,
+  maxPagesToCrawl: 9999999999,
   outputFileName: "north-korea-books.json",
   maxTokens: 2000000,
   selector: "body",
