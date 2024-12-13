90a0424da0ebe6879d38ee3776895136f7913f88
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 01:50:02 +0000

    uf

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "http://aindf.dyndns.org/kuguk8/Title1.htm",
   match: "http://aindf.dyndns.org/kuguk8/Title1.htm/**",
-  maxPagesToCrawl: 99999999999,
+  maxPagesToCrawl: 9999999999,
   outputFileName: "aindf.json",
   maxTokens: 2000000,
   selector: "body",
