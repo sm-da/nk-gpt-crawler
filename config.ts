40f21465133da03b92e910c1750328280dbaf6bd
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 00:01:40 +0000

    Update file

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "http:/",
-  match: "http://**",
+  url: "http://175.45.176.14/ko/notice/cholsan/",
+  match: "http://175.45.176.14/ko/notice/cholsan/**",
   maxPagesToCrawl: 99999999999,
   outputFileName: ".json",
   maxTokens: 2000000,
