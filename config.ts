48ac755fd56be4faa2c9eebf8ac8b25c7c2dc5b1
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 06:58:10 +0000

    uf

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https:/sec.gov/",
-  match: "https:/sec.gov/**",
+  url: "https://www.sec.gov/",
+  match: "https://www.sec.gov/**",
   maxPagesToCrawl: 99999999999,
   outputFileName: "sec.json",
   maxTokens: 2000000,
