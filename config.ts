4284da13db6d4db88f19b84c9e552bcad008ef14
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 00:18:21 +0000

    uf

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "http://chosonsinbo.com/",
   match: "http://chosonsinbo.com/**",
-  maxPagesToCrawl: 99999999999,
+  maxPagesToCrawl: 9999999999,
   outputFileName: "chosonsinbo.json",
   maxTokens: 2000000,
   selector: "body",
