86e672c7849753635de5ff4748305d7eb59bef25
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 00:37:31 +0000

    uf

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "http://www.gnu.rep.kp/",
   match: "http://www.gnu.rep.kp/**",
-  maxPagesToCrawl: 9999999999,
+  maxPagesToCrawl: 999999999,
   outputFileName: ".json",
   maxTokens: 2000000,
   selector: "body",
