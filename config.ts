ad9fe19d5586afcdabda99211706ef2c2abe96cd
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 01:18:40 +0000

    uf

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "http:/",
-  match: "http://**",
+  url: "http://www.cooks.org.kp/	",
+  match: "http://www.cooks.org.kp/**",
   maxPagesToCrawl: 99999999999,
   outputFileName: ".json",
   maxTokens: 2000000,
