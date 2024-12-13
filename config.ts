314c1e8210bb11d84411ff7c160a18443ea9e698
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 02:12:24 +0000

    uf

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "http://175.45.176.14/ko/notice/2009web/homepage.html",
   match: "http://175.45.176.14/ko/notice/2009web/homepage.html/**",
-  maxPagesToCrawl: 99999999999,
+  maxPagesToCrawl: 9999999999,
   outputFileName: "pitf.json",
   maxTokens: 2000000,
   selector: "body",
