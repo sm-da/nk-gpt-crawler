dff33d769924739203f6e7cddc1b451320a2ed9b
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 01:43:46 +0000

    uf

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "http://ndfsk.dyndns.org/",
   match: "http://ndfsk.dyndns.org/**",
-  maxPagesToCrawl: 99999999999,
+  maxPagesToCrawl: 9999999999,
   outputFileName: "ndfsk.json",
   maxTokens: 2000000,
   selector: "body",
