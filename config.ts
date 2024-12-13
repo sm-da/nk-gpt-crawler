0808b5757bb7e2853c912b9485acccb18372771d
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 01:59:13 +0000

    uf

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "http:/",
-  match: "http://**",
+  url: "http://www.northkoreatech.org/",
+  match: "http://www.northkoreatech.org/**",
   maxPagesToCrawl: 99999999999,
   outputFileName: ".json",
   maxTokens: 2000000,
