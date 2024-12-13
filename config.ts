d8f8897c08502568fe8b02c9d434013acc897f56
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 01:38:05 +0000

    uf

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "http://www.minjok.com/",
   match: "http://www.minjok.com/**",
-  maxPagesToCrawl: 9999999999,
+  maxPagesToCrawl: 999999999,
   outputFileName: "minjok.json",
   maxTokens: 2000000,
   selector: "body",
