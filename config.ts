7e41c000d6f554b0849470208c196d27ea9cdcb4
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 00:20:59 +0000

    uf

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "http://www.elufa.net/",
   match: "http://www.elufa.net/**",
-  maxPagesToCrawl: 99999999999,
+  maxPagesToCrawl: 9999999999,
   outputFileName: "elufa.json",
   maxTokens: 2000000,
   selector: "body",
