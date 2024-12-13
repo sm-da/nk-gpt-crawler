d53915b844f176595ee94c44a187148167299792
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 01:49:02 +0000

    uf

--- config.ts
+++ config.ts
@@ -4,7 +4,7 @@ export const defaultConfig: Config = {
   url: "http://aindf.dyndns.org/kuguk8/Title1.htm",
   match: "http://aindf.dyndns.org/kuguk8/Title1.htm/**",
   maxPagesToCrawl: 99999999999,
-  outputFileName: ".json",
+  outputFileName: "aindf.json",
   maxTokens: 2000000,
   selector: "body",
 };
