06e048231173d9c4d7d5facc9f0806e5059ed051
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 00:03:11 +0000

    Update file

--- config.ts
+++ config.ts
@@ -4,7 +4,7 @@ export const defaultConfig: Config = {
   url: "http://175.45.176.14/ko/notice/cholsan/",
   match: "http://175.45.176.14/ko/notice/cholsan/**",
   maxPagesToCrawl: 99999999999,
-  outputFileName: ".json",
+  outputFileName: "cholsan.json",
   maxTokens: 2000000,
   selector: "body",
 };
