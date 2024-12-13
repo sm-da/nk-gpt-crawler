1d2827ebf5621921709b1ed61c6f42cb817cacdf
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 13:14:36 +0000

    uf

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "https://oshkoshdefense.com/",
   match: "https://oshkoshdefense.com/**",
-  maxPagesToCrawl: 99999999999,
+  maxPagesToCrawl: 9999999999,
   outputFileName: "oshkoshdefense.json",
   maxTokens: 2000000,
   selector: "body",
