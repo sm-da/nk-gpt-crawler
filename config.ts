2be5c81110c1edd1d8f4d2b55e2832877c1e69d4
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 02:13:20 +0000

    uf

--- config.ts
+++ config.ts
@@ -1,10 +1,10 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "http:/",
-  match: "http://**",
+  url: "http://www.rodong.rep.kp/",
+  match: "http://www.rodong.rep.kp/**",
   maxPagesToCrawl: 99999999999,
-  outputFileName: ".json",
+  outputFileName: "rodong.json",
   maxTokens: 2000000,
   selector: "body",
 };
