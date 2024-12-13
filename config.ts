cc4034b25f0451e106526ccbb907ef46181dcbed
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 00:06:09 +0000

    UPdate file

--- config.ts
+++ config.ts
@@ -1,10 +1,10 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "http:/",
-  match: "http://**",
+  url: "http://www.chongryon.com/*",
+  match: "http://www.chongryon.com/**",
   maxPagesToCrawl: 99999999999,
-  outputFileName: ".json",
+  outputFileName: "chongron-com.json",
   maxTokens: 2000000,
   selector: "body",
 };
